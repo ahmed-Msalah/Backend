47b9352b46c5058c29d05f41fd3f57537a6d95be
ali ismail <aliismailh08@gmail.com>
2025-01-29 22:29:08 +0200

    add Not Found Route

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ app.use(express.json());
 // Routes
 app.use('/api', authRouter);
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Endpoint not found" });
+});
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
