9a6cbeeaa2345f56868113b02b9fd92293a61336
Ahmed Salah <Ahmed Salah>
2025-02-15 16:59:52 +0200

    finish set up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRouter = require('./src/routes/auth.route.js')
+const authRouter = require('./src/routes/auth.route.js');
 
 dotenv.config();
 
@@ -11,19 +11,19 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 
-
 // Routes
 app.use('/api', authRouter);
 
 app.use((req, res, next) => {
-    res.status(404).json({ message: "Endpoint not found" });
+  res.status(404).json({ message: 'Endpoint not found' });
 });
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('Connected to MongoDB');
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    })
-    .catch((error) => console.error('MongoDB connection error:', error));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(error => console.error('MongoDB connection error:', error));
 console.log(process.env);
