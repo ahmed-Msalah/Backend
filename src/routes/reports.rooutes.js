8a91ca1ad6b681bb47061404e78e9fbcb28be972
Ahmed Salah <Ahmed Salah>
2025-03-06 04:17:47 +0200

    add 3 endpoints in repor.controller

--- src/routes/reports.rooutes.js
+++ src/routes/reports.rooutes.js
@@ -1,11 +1,19 @@
-const express = require("express");
-const { authenticateToken } = require("../middleware/authorized.middleware");
-const { getUsageForDeviceByUser, getAllUsageForUser } = require("../controller/repor.controller");
+const express = require('express');
+const { authenticateToken } = require('../middleware/authorized.middleware');
+const {
+  getUsageForDeviceByUser,
+  getAllUsageForUser,
+  totalConsumptionForUser,
+  CompareSavingPercentage,
+  highestDeviceConsume,
+} = require('../controller/repor.controller');
 
 const Route = express.Router();
 
-Route.get("/user/:userId", authenticateToken, getAllUsageForUser);
-Route.get("/user/:userId/:deviceId",authenticateToken, getUsageForDeviceByUser);
-
+Route.get('/user/:userId', authenticateToken, getAllUsageForUser);
+Route.get('/user/:userId/:deviceId', authenticateToken, getUsageForDeviceByUser);
+Route.get('/consumption/total/:userId', totalConsumptionForUser);
+Route.get('/consumption/savings/:userId', CompareSavingPercentage);
+Route.get('/consumption/highest/:userId', highestDeviceConsume);
 
 module.exports = Route;
