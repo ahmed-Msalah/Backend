cd59a7b8f0d432e53f8802b3cccfcebb2cf379dc
Ahmed Salah <Ahmed Salah>
2025-04-12 23:00:56 +0200

    add avg usage

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -1,14 +1,12 @@
-const Category = require("../models/category.model");
-
+const Category = require('../models/category.device.model');
 
 const getDeviceCategories = async (req, res) => {
-
-    try {
-        const categories = await Category.find({});
-        res.status(200).json({ categories })
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const categories = await Category.find({});
+    res.status(200).json({ categories });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = getDeviceCategories;
