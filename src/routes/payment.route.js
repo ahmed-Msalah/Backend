6280c29363c4efdd4fc44a3b20854640a0bcce7d
ali ismail <aliismailh08@gmail.com>
2025-06-08 14:36:40 +0300

    feat(payment): add payment feature using stripe2

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { handleStripeWebhook,  initPayament} = require('../controller/payment.controller');
+const { authenticateToken } = require('../middleware/authorized.middleware');
 
-router.post('/create', initPayament);
-router.post('/webhook', handleStripeWebhook);
+router.post('/create', authenticateToken, initPayament);
+router.post('/webhook',authenticateToken, handleStripeWebhook);
 
 module.exports = router;
