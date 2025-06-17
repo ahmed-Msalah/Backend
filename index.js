fd23c819e8c248ca5bdcf2864d3876fbe7e9855d
Ahmed Salah <Ahmed Salah>
2025-06-17 23:59:11 +0300

    add connection string

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const appInsights = require('applicationinsights');
+appInsights
+  .setup(
+    'InstrumentationKey=62bb47fe-3931-45b6-af57-c2adca117833;IngestionEndpoint=https://uaenorth-0.in.applicationinsights.azure.com/;LiveEndpoint=https://uaenorth.livediagnostics.monitor.azure.com/;ApplicationId=5578d9fe-bdc6-4123-b772-a6ab26821765',
+  )
+  .start();
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -12,9 +18,8 @@ const automationRouter = require('./src/routes/automation.route.js');
 const paymentRouter = require('./src/routes/payment.route.js');
 const notificationRouter = require('./src/routes/notification.route.js');
 const { client } = require('./src/mqtt/connection.js');
-require("./src/buit.in.automation/power.saving.mode.js");
+require('./src/buit.in.automation/power.saving.mode.js');
 const bodyParser = require('body-parser');
-
 
 dotenv.config();
 
@@ -25,33 +30,32 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // MQTT
 setTimeout(() => {
   if (client.connected) {
-    console.log("🟢 MQTT is connected and running.");
+    console.log('🟢 MQTT is connected and running.');
   } else {
-    console.log("🔴 MQTT is not connected.");
+    console.log('🔴 MQTT is not connected.');
   }
 }, 2000);
 
-app.post('/api/payment/webhook',
+app.post(
+  '/api/payment/webhook',
   bodyParser.raw({ type: 'application/json' }),
-  require('./src/controller/payment.controller.js').handleStripeWebhook
+  require('./src/controller/payment.controller.js').handleStripeWebhook,
 );
 
 // Routes
 app.use('/api', authRouter);
 app.use('/api/users', userRouter);
-app.use("/api/reports", reportRouter);
-app.use("/api/room", roomRouter);
-app.use("/api/device", deviceRouter);
-app.use("/api/category", categoryRouter);
-app.use("/api/sensor", sensorRouter);
-app.use("/api/automation", automationRouter);
-app.use("/api/payment", paymentRouter);
-app.use("/api/notification", notificationRouter);
-
+app.use('/api/reports', reportRouter);
+app.use('/api/room', roomRouter);
+app.use('/api/device', deviceRouter);
+app.use('/api/category', categoryRouter);
+app.use('/api/sensor', sensorRouter);
+app.use('/api/automation', automationRouter);
+app.use('/api/payment', paymentRouter);
+app.use('/api/notification', notificationRouter);
 
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Endpoint not found' });
