26859617f8720d3b65084ea359ac68b9632a026d
Ahmed Salah <Ahmed Salah>
2025-06-19 11:18:14 +0300

    fix(payment)

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { handleStripeWebhook,  initPayament, sucessPageServe, failedPageServe} = require('../controller/payment.controller');
+const {
+  handleStripeWebhook,
+  initPayament,
+  sucessPageServe,
+  failedPageServe,
+} = require('../controller/payment.controller');
 const { authenticateToken } = require('../middleware/authorized.middleware');
 
-router.post('/create', initPayament);
-router.post('/webhook',authenticateToken, handleStripeWebhook);
+router.post('/create', authenticateToken, initPayament);
+router.post('/webhook', authenticateToken, handleStripeWebhook);
 router.get('/payment-success', sucessPageServe);
 router.get('/payment-failed', failedPageServe);
 
